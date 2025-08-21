9f1edba62db2466352f028ac265bc658a52eb3ec
VSHAGEETHPRATHEEP06 <shagith06@gmail.com>
2025-08-21 14:45:31 +0530

    Version 2

--- backend/middleware/author.js
+++
@@ -1,8 +0,0 @@
-const requireAuthor = (req, res, next) => {
-  if (!req.user?.isAuthor) {
-    return res.status(403).json({ error: 'Author access required' });
-  }
-  next();
-};
-
-export default requireAuthor; 
